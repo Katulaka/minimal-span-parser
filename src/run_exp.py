3fea3df3e0ead39b031058d50d522002625c50fc
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-10-05 23:46:05 -0700

    run exp on dropout

--- src/run_exp.py
+++ src/run_exp.py
@@ -5,31 +5,26 @@ import itertools
 proc = []
 n_gpus = 8
 embed_dim = [64, 128, 256, 512]
-embed_iter = itertools.product(embed_dim[:-2], embed_dim[:-2], embed_dim[2:], embed_dim[2:])
-for i, dims in enumerate(embed_iter):
-    path = ("models_grid_search/char[-]_lstms[0.1]_embeddings[0.4]"
-            "_temb[{}]_cemb[{}]_wemb[{}]_lemb[{}]").format(*dims)
+dropouts = [0.1, 0.2, 0.3, 0.4]
+iter = itertools.product(dropouts, dropouts)
+for i, drops in enumerate(iter):
+    path = ("models_grid_search/char[+]_lstms[{}]_embeddings[{}]"
+            "_temb[64]_cemb[64]_wemb[256]_lemb[256]").format(*drops)
     device = 'GPU:{}'.format(i%n_gpus)
     command = ("python src/main.py train "
                 "--use-char-lstm --epochs 10 --parser-type my "
-                "--dropouts 0.1 0.4 "
+                "--dropouts {} {} "
                  "--dynet-mem 2048 --dynet-autobatch 1 "
                  "--model-path-base {} "
                 "--dynet-devices {} "
-                "--tag-embedding-dim {} "
-                "--char-embedding-dim {} "
-                "--word-embedding-dim {} "
-                "--label-embedding-dim {} "
-                 ).format(path, device, *dims)
+                "--tag-embedding-dim 64 "
+                "--char-embedding-dim 64 "
+                "--word-embedding-dim 256 "
+                "--label-embedding-dim 256 "
+                # "--char-lstm-dim {100} "
+                # "--lstm-dim {250} "
+                # "--dec-lstm-dim {600} "
+                # "--attention-dim {250} "
+                # "--label-hidden-dim {250} "
+                 ).format(path, *drops, device)
     proc.append(Popen(command.split()))
-
-    #
-    #
-    #
-    #
-    #
-    # "--char-lstm-dim 100"
-    # "--lstm-dim 250"
-    # "--dec-lstm-dim 600"
-    # "--attention-dim 250"
-    # "--label-hidden-dim 250"
