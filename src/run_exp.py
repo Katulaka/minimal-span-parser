4063d1ab2ef3d91e9578a9d470228ff8d02bbbb3
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-10-14 14:53:24 -0700

    fixed typo

--- src/run_exp.py
+++ src/run_exp.py
@@ -6,7 +6,7 @@ for i, embeddings in enumerate(itertools.product([50, 100, 150], [100, 150, 200]
     if embeddings != (50, 100):
         device = 'GPU:{}'.format(i%n_gpus)
         command = ("python src/main.py train "
-                    "--use-char-lstm --parser-type my --model-path-base run_exp"
+                    "--use-char-lstm --parser-type my --model-path-base run_exp "
                     "--dynet-mem 2048 --dynet-autobatch 1 "
                     # "--dropouts {} {} "
                     "--tag-embedding-dim {} "
@@ -20,5 +20,4 @@ for i, embeddings in enumerate(itertools.product([50, 100, 150], [100, 150, 200]
                     # "--label-hidden-dim {} "
                     "--dynet-devices {} "
                      ).format(*embeddings, device)
-        import pdb; pdb.set_trace()
         Popen(command.split())
