11b3793acfb24af6f32c5c178529a936cd614371
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-11-27 09:30:55 -0800

    run exp for model with valance values

--- src/run_exp.py
+++ src/run_exp.py
@@ -2,24 +2,23 @@ from subprocess import Popen
 import itertools
 
 n_gpus = 8
-# for i, embeddings in enumerate(itertools.product([200, 250, 300], [100, 150, 200])):
-#     if embeddings != (250, 100):
-# for i, embedding in enumerate([500,700,800,900,1000]):
-for i, dropouts in  enumerate(itertools.product([0.1, 0.2, 0.3, 0.5], [0, 0.1, 0.2, 0.3, 0.4, 0.5])):
-    device = 'GPU:{}'.format(i%n_gpus)
-    command = ("python src/main.py train "
-                "--use-char-lstm --parser-type my --model-path-base run_exp "
-                "--dynet-mem 2048 --dynet-autobatch 1 "
-                "--dropouts {} {} "
-                # "--tag-embedding-dim {} "
-                # "--word-embedding-dim {} "
-                # "--char-embedding-dim {} "
-                # "--label-embedding-dim {} "
-                # "--char-lstm-dim {} "
-                # "--lstm-dim {} "
-                # "--dec-lstm-dim {} "
-                # "--attention-dim {} "
-                # "--label-hidden-dim {} "
-                "--dynet-devices {} "
-                 ).format(*dropouts, device)
+for i, dropouts in  enumerate(itertools.product([0.1, 0.2, 0.3, 0.4, 0.5], [0, 0.1, 0.2, 0.3, 0.4, 0.5])):
+    if dropouts!=(0.4, 0.4):
+        device = 'GPU:{}'.format(i%n_gpus)
+        command = ("python src/main.py train "
+                    "--keep-valence-value "
+                    "--use-char-lstm --parser-type my --model-path-base run_exp "
+                    "--dynet-mem 2048 --dynet-autobatch 1 "
+                    "--dropouts {} {} "
+                    # "--tag-embedding-dim {} "
+                    # "--word-embedding-dim {} "
+                    # "--char-embedding-dim {} "
+                    # "--label-embedding-dim {} "
+                    # "--char-lstm-dim {} "
+                    # "--lstm-dim {} "
+                    # "--dec-lstm-dim {} "
+                    # "--attention-dim {} "
+                    # "--label-hidden-dim {} "
+                    "--dynet-devices {} "
+                     ).format(*dropouts, device)
     Popen(command.split())
