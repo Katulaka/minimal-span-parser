e103aa236c5f40b19835ec0e58d3896d9e286d71
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-11-28 20:32:33 -0800

    grid search bidi-char

--- src/run_exp.py
+++ src/run_exp.py
@@ -2,25 +2,24 @@ from subprocess import Popen
 import itertools
 
 n_gpus = 8
-tag_embedding = [150, 200]
-word_embedding = [100, 150]
-char_embedding = [50, 100]
-for i, embeddings in enumerate(itertools.product(tag_embedding, word_embedding, char_embedding)):
-    if embeddings != (150, 100, 50):
+label_embedding = [100, 150, 200]
+char_lstm = [100, 150, 200]
+for i, dims in enumerate(itertools.product(label_embedding, char_lstmg)):
+    if dims != (100, 100):
         device = 'GPU:{}'.format(i%n_gpus)
         command = ("python src/main.py train "
                     "--use-char-lstm --parser-type my --model-path-base run_exp "
                     "--dynet-mem 2048 --dynet-autobatch 1 "
                     # "--dropouts {} {} "
-                    "--tag-embedding-dim {} "
-                    "--word-embedding-dim {} "
-                    "--char-embedding-dim {} "
-                    # "--label-embedding-dim {} "
-                    # "--char-lstm-dim {} "
+                    "--tag-embedding-dim 200 "
+                    "--word-embedding-dim 100 "
+                    "--char-embedding-dim 50 "
+                    "--label-embedding-dim {} "
+                    "--char-lstm-dim {} "
                     # "--lstm-dim {} "
                     # "--dec-lstm-dim {} "
                     # "--attention-dim {} "
                     # "--label-hidden-dim {} "
                     "--dynet-devices {} "
-                     ).format(*embeddings, device)
+                     ).format(*dims, device)
         Popen(command.split())
