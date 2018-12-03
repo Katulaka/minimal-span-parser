d13c13330048f88bb23e0baf2ffdd6b3f8ac5ce7
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-12-02 18:40:00 -0800

    run exp from gpu 1

--- src/run_exp.py
+++ src/run_exp.py
@@ -8,22 +8,21 @@ char_embedding = [50, 100]
 label_embedding = [250, 300]
 label_dim = [500, 600, 700]
 for i, dims in enumerate(itertools.product(label_embedding, label_dim)):
-    if dims != (100, 600):
-        device = 'GPU:{}'.format(i%n_gpus)
-        command = ("python src/main.py train "
-                    "--keep-valence-value "
-                    "--use-char-lstm --parser-type my --model-path-base run_exp "
-                    "--dynet-mem 2048 --dynet-autobatch 1 "
-                    "--dropouts 0.4 0.3 "
-                    "--tag-embedding-dim 150 "
-                    "--word-embedding-dim 150 "
-                    "--char-embedding-dim 50 "
-                    "--label-embedding-dim {} "
-                    # "--char-lstm-dim {} "
-                    # "--lstm-dim {} "
-                    "--dec-lstm-dim {} "
-                    # "--attention-dim {} "
-                    # "--label-hidden-dim {} "
-                    "--dynet-devices {} "
-                     ).format(*dims, device)
-        Popen(command.split())
+    device = 'GPU:{}'.format(i%n_gpus+1)
+    command = ("python src/main.py train "
+                "--keep-valence-value "
+                "--use-char-lstm --parser-type my --model-path-base run_exp "
+                "--dynet-mem 2048 --dynet-autobatch 1 "
+                "--dropouts 0.4 0.3 "
+                "--tag-embedding-dim 150 "
+                "--word-embedding-dim 150 "
+                "--char-embedding-dim 50 "
+                "--label-embedding-dim {} "
+                # "--char-lstm-dim {} "
+                # "--lstm-dim {} "
+                "--dec-lstm-dim {} "
+                # "--attention-dim {} "
+                # "--label-hidden-dim {} "
+                "--dynet-devices {} "
+                 ).format(*dims, device)
+    Popen(command.split())
