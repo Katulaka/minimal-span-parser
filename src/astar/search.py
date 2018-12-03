50faede1340ef0ce7ad610a245c44f49b85a78a6
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-12-03 14:19:46 -0800

    fixed syntax error

--- src/astar/search.py
+++ src/astar/search.py
@@ -167,13 +167,13 @@ class Solver(AStar):
     def is_goal_reached(self, node, goal):
         # if (node.left, node.right) == (goal.left, goal.right):
             # return not len(list(node.tree.missing_leaves()))
-        if (node.left, node.right) == (goal.left, goal.right)
+        if (node.left, node.right) == (goal.left, goal.right) \
             and not len(list(node.tree.missing_leaves())):
             node_leaves = list(node.tree.leaves())
             goal_leaves = list(goal.tree.leaves())
             return all(
                 (goal_leaf.tag, goal_leaf.word) == (node_leaf.tag, node_leaf.word)
-                for goal_leaf, node_leaf in zip(goal_leaves, node_leaves))            
+                for goal_leaf, node_leaf in zip(goal_leaves, node_leaves))
         return False
 
 def astar_search(grid, sentence, keep_valence_value, astar_parms):
