a898165f922d9cddc473ea10bc919fa65e78720a
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-12-03 12:43:32 -0800

    move break point

--- src/astar/search.py
+++ src/astar/search.py
@@ -178,6 +178,4 @@ def astar_search(grid, keep_valence_value, astar_parms):
     solver = Solver(grid, keep_valence_value)
     nodes = solver.astar(start, goal, *astar_parms)
 
-    import pdb; pdb.set_trace()
-
     return nodes, solver.seen
