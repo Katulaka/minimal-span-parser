1ae2f46232c47017dc46d3c97c135ea7ad1230ec
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-12-05 18:42:39 -0800

    new is_valid fix

--- src/astar/search.py
+++ src/astar/search.py
@@ -66,12 +66,13 @@ class AstarNode(object):
                         if label == leaf.label.split(miss_side)[-1]:
                             break
                 try:
-                    self.tree = _trees[1].combine(_trees[0].children[0], leaf)
-                    return True
+                    tree = _trees[1].combine(_trees[0].children[0], leaf)
                     # return _trees[1].combine(_trees[0].children[0], leaf)
                 except:
                     import pdb; pdb.set_trace()
                     return False
+
+                self.tree = tree
 
             return False
 
