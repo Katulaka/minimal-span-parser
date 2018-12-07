2afaa30a85cc94c7eebf0448cb924af38b518a51
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-12-07 01:14:22 -0800

    try mitchell parser

--- src/parse.py
+++ src/parse.py
@@ -349,7 +349,8 @@ class MyParser(object):
         self.dec_lstm = dy.LSTMBuilder(
             1,
             label_embedding_dim,
-            dec_lstm_dim,
+            # dec_lstm_dim,
+            2 * lstm_dim,
             self.model)
 
         enc_out_dim = embedding_dim + 2 * lstm_dim
@@ -357,7 +358,8 @@ class MyParser(object):
         Weights = collections.namedtuple('Weights', 'name prev_dim next_dim')
         ws = []
         ws.append(Weights(name='c_dec', prev_dim=enc_out_dim, next_dim=dec_lstm_dim))
-        ws.append(Weights(name='key', prev_dim=dec_lstm_dim, next_dim=attention_dim))
+        # ws.append(Weights(name='key', prev_dim=dec_lstm_dim, next_dim=attention_dim))
+        ws.append(Weights(name='key', prev_dim=2 * lstm_dim, next_dim=attention_dim))
         ws.append(Weights(name='query', prev_dim=dec_lstm_dim, next_dim=attention_dim))
         ws.append(Weights(name='attention', prev_dim=dec_attend_dim, next_dim=label_hidden_dim))
         ws.append(Weights(name='probs', prev_dim=label_hidden_dim, next_dim=label_vocab.size))
@@ -419,8 +421,9 @@ class MyParser(object):
                 embeddings.append(dy.concatenate([tag_embedding, word_embedding]))
         lstm_outputs = self.enc_lstm.transduce(embeddings)
 
-        encode_outputs_list = [dy.affine_transform([*self.ws['c_dec'], dy.concatenate([e, l])])
-                                    for e, l in zip(embeddings[1:-1], lstm_outputs[1:-1])]
+        # encode_outputs_list = [dy.affine_transform([*self.ws['c_dec'], dy.concatenate([e, l])])
+        #                             for e, l in zip(embeddings[1:-1], lstm_outputs[1:-1])]
+        encode_outputs_list = lstm_outputs
 
         if is_train:
             decode_inputs = [(START,) + tuple(leaf.labels) + (STOP,) for leaf in gold.leaves()]
