be66ded0cb8169111ff69a4e3a6d81504ad61bd3
Katia Patkin <katia.patkin@EECS.Berkeley.EDU>
2018-12-14 02:56:20 -0800

    improve run time of chart astar search

--- src/astar/search_chart.py
+++ src/astar/search_chart.py
@@ -17,7 +17,7 @@ class AstarNode(object):
         assert isinstance(split, int)
         self.split = split
 
-        assert isinstance(rank, list)
+        assert isinstance(rank, tuple)
         self.rank = rank
 
         left_rank, right_rank, label_rank = rank
@@ -51,7 +51,7 @@ class Solver(AStar):
     def __init__(self, grid, chart):
         self.grid = grid
         self.chart = chart
-        self.cl = []
+        self.cl = {}
 
     def heuristic_cost(self, node, goal, cost_coefficient):
         return 0
@@ -65,32 +65,44 @@ class Solver(AStar):
         return real_cost + heuristic_cost
 
     def move_to_closed(self, node):
-        self.cl.append(node)
+        # self.cl.append(node)
+
+        self.cl[node.left, node.right, node.split, node.rank] = node
         return True
 
     def neighbors(self, node):
 
         neighbors = []
-
-        rank = list(np.array(node.rank) + np.array([1, 0, 0]))
-        if rank[0] < len(self.chart[node.left, node.split]):
-            neighbor = AstarNode(node.left, node.right, node.split, rank, self.chart, self.grid)
-            if neighbor not in self.cl:
-                neighbors.append(neighbor)
-
-        rank = list(np.array(node.rank) + np.array([0, 1, 0]))
-        if rank[1] < len(self.chart[node.split, node.right]):
-            neighbor = AstarNode(node.left, node.right, node.split, rank, self.chart, self.grid)
-            if neighbor not in self.cl:
-                neighbors.append(neighbor)
-
-        rank = list(np.array(node.rank) + np.array([0, 0, 1]))
-        if rank[2] < len(self.grid[node.left, node.right]):
-            neighbor = AstarNode(node.left, node.right, node.split, rank, self.chart, self.grid)
-            if neighbor not in self.cl:
-                neighbors.append(neighbor)
-
+        for i, delta in enumerate(np.eye(3)):
+            rank = tuple(np.array(node.rank) + delta)
+            if rank[i] < len(self.chart[node.left, node.split]):
+                nb_key = (node.left, node.right, node.split, rank)
+                neighbor = AstarNode(*nb_key, self.chart, self.grid)
+                if nb_key not in self.cl:
+                    neighbors.append(neighbor)
         return neighbors
+
+        # rank = tuple(np.array(node.rank) + np.array([1, 0, 0]))
+        # if rank[0] < len(self.chart[node.left, node.split]):
+        #     nb_key = (node.left, node.right, node.split, rank)
+        #     neighbor = AstarNode(*nb_key, self.chart, self.grid)
+        #     if nb_key not in self.cl:
+        #         neighbors.append(neighbor)
+
+        # rank = tuple(np.array(node.rank) + np.array([0, 1, 0]))
+        # if rank[1] < len(self.chart[node.split, node.right]):
+        #     nb_key = (node.left, node.right, node.split, rank)
+        #     neighbor = AstarNode(*nb_key, self.chart, self.grid)
+        #     if nb_key not in self.cl:
+        #         neighbors.append(neighbor)
+        #
+        # rank = tuple(np.array(node.rank) + np.array([0, 0, 1]))
+        # if rank[2] < len(self.grid[node.left, node.right]):
+        #     nb_key = (node.left, node.right, node.split, rank)
+        #     neighbor = AstarNode(*nb_key, self.chart, self.grid)
+        #     if nb_key not in self.cl:
+        #         neighbors.append(neighbor)
+
 
     def is_goal_reached(self, node, goal):
         return (node.left, node.right) == goal
